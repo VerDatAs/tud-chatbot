8de988c4934b8b6a5957940282bf2ccabcd7c969
Tommy Kubica <tommy.kubica@tu-dresden.de>
2024-01-01 20:11:01 +0100

    Add workaround for parsing Array timestamps;

--- src/util/assistanceObjectHelper.ts
+++ src/util/assistanceObjectHelper.ts
@@ -6,8 +6,17 @@ export const parameterValue = (message: AssistanceObjectCommunication, key: stri
 };
 
 // Retrieved from https://jerickson.net/how-to-format-dates-in-vue-3/
-export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
-  // Then specify how you want your dates to be formatted
-  return new Intl.DateTimeFormat('default', { dateStyle: 'short', timeStyle: 'short' }).format(date);
+export const formatDate = (dateTime: any) => {
+  // Retrieve dateTime from dateTimeString: https://stackoverflow.com/a/1353710
+  const timestamp = Date.parse(dateTime);
+  if (!isNaN(timestamp)) {
+    const dateTimestamp = new Date(timestamp);
+    return new Intl.DateTimeFormat('default', { dateStyle: 'short', timeStyle: 'short' }).format(dateTimestamp);
+  } else if (Array.isArray(dateTime)) {
+    // Workaround for current implementation of the dateTimeString: [2023,12,30,15,35,14,469304000]
+    const dateTimestamp = new Date(dateTime[0], dateTime[1], dateTime[2], dateTime[3], dateTime[4], dateTime[5]);
+    return new Intl.DateTimeFormat('default', { dateStyle: 'short', timeStyle: 'short' }).format(dateTimestamp);
+  } else {
+    return 'Invalid date';
+  }
 };
